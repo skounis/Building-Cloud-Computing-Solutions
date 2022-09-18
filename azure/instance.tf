e226dc4f869289398e7355d30074dfae7172a31f
Stavros Kounis <skounis@gmail.com>
2022-09-18 11:00:25 +0200

    Update instance.tf

--- azure/instance.tf
+++ azure/instance.tf
@@ -114,7 +114,11 @@ resource "tls_private_key" "example_ssh" {
     algorithm = "RSA"
     rsa_bits = 4096
 }
-output "tls_private_key" { value = tls_private_key.example_ssh.private_key_pem }
+
+output "tls_private_key" { 
+    value = tls_private_key.example_ssh.private_key_pem 
+    sensitive = true    
+}
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "myterraformvm" {
