78f464c363ce4c96c2be3cc2d5f0b7ffd0c80ed7
Stavros Kounis <skounis@gmail.com>
2022-09-18 11:22:47 +0200

    Output public ip

--- azure/instance.tf
+++ azure/instance.tf
@@ -115,6 +115,10 @@ resource "tls_private_key" "example_ssh" {
     rsa_bits = 4096
 }
 
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.myterraformvm.public_ip_address
+}
+
 output "tls_private_key" { 
     value = tls_private_key.example_ssh.private_key_pem 
     sensitive = true    
